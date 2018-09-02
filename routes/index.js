1d1c38a75007e5a6dd44f2963e9202bb82cb7bd4
zhao jingyu <dlzhaojy@cn.ibm.com>
2018-09-02 12:01:02 +0800

    add ServerSendEvents.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/sse', function (req, res, next) {
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    });
+    var timer = setInterval(() => {
+        var date = new Date();
+        console.log(date);
+        res.write('data: {"date": "' + date + '"}\n\n');
+    }, 2000);
+    req.connection.addListener('close', () => {
+        clearInterval(timer);
+        console.log('====CLOSE====');
+    }, false);
 });
 
 module.exports = router;
